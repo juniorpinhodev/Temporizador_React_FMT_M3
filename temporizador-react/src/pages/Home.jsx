bb85e73c858db3c8961c5345311aca2faa8adbc6
juniorpinhodev <juniorpinhodev@gmail.com>
2024-08-29 18:52:21 -0300

    feat: 🎸 Cria Contexto de autenticação

--- temporizador-react/src/pages/Home.jsx
+++ temporizador-react/src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+
 function Home() {
+    const { user, logout } = useContext(AuthContext);
+
     return (
       <div>
-        <h2>Bem-vindo à Página Inicial</h2>
+        <h1>Bem-vindo à Página Inicial</h1>
         <p>Este é o conteúdo da página inicial.</p>
+
+        {user ? (
+        <div>
+          <p>Usuário logado: {user.email}</p>
+          <button onClick={logout}>Logout</button>
+        </div>
+      ) : (
+        <p>Você não está logado.</p>
+      )}
       </div>
     );
   }
--- temporizador-react/src/pages/Login.jsx
+++ temporizador-react/src/pages/Login.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 
 function Login() {
+  const { login } = useContext(AuthContext); // Acessando a função login do contexto
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log({ email, password });
+    login(email, password); // Chamando a função de login do contexto
+    navigate('/'); // Redireciona para a página Home após o login
   };
 
   return (
